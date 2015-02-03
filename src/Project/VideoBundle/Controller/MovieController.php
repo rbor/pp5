af6ec7fa5a66aff06d5ef1c2714e5b9dbee0d137
Marcin Wegrzyn <mar512@interia.pl>
2015-02-03 21:00:14 +0100

    Username w /

--- src/Project/VideoBundle/Controller/MovieController.php
+++ src/Project/VideoBundle/Controller/MovieController.php
@@ -15,7 +15,11 @@ class MovieController extends Controller
 		$title = $data['Title'];
 		$description = $data['Plot'];
 		$actors = $data['Actors'];
-		$poster = $data['Poster'];
+        
+        if($data['Poster'] != 'N/A')
+            $poster = $data['Poster'];
+        else
+            $poster = null;
 
         return $this->render('ProjectVideoBundle:Movie:movie.html.twig', array(
         	'title' 	=> $title,
@@ -24,4 +28,45 @@ class MovieController extends Controller
         	'poster' 	=> $poster
         	 ));
     }
+
+    public function idAction($movie)
+    {
+        $url = 'http://www.omdbapi.com/?i='.$movie.'&y=&plot=full&r=json';
+        $json = file_get_contents($url);
+        $data = json_decode($json, TRUE);
+
+        $title = $data['Title'];
+        $description = $data['Plot'];
+        $actors = $data['Actors'];
+
+        if($data['Poster'] != 'N/A')
+            $poster = $data['Poster'];
+        else
+            $poster = null;
+
+        return $this->render('ProjectVideoBundle:Movie:movie.html.twig', array(
+            'title'     => $title,
+            'desc'      => $description,
+            'actors'    => $actors,
+            'poster'    => $poster
+             ));
+    }
+
+    public function searchAction($movie)
+    {
+        $url = 'http://www.omdbapi.com/?s='.$movie.'&y=&type=movie&r=json';
+        $json = file_get_contents($url);
+        $data = json_decode($json, TRUE);
+
+        $list = $data['Search'];
+
+        return $this->render('ProjectVideoBundle:Movie:search.html.twig', array('list' => $list));
+    }
+
+
+    public function findAction()
+    {
+        return $this->render('ProjectVideoBundle:Movie:find.html.twig');
+    }
+
 }
