91ba3d1173293450679d21f70faf0e1925c6d07b
rbor <root@brad-ThinkPad-X131e.(none)>
2015-02-04 22:29:28 +0100

    add PaymentController

---
+++ src/Project/VideoBundle/Controller/PaymentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Project\VideoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PaymentController extends Controller
+{
+    public function payAction()
+    {
+        $data=array(
+			'id' => 72890,
+			'amount' => 222.52,
+			'currency' => 'PLN',
+			'description' => 'Zaplata',
+			'control' => 'FV-153255',
+			'firstname' => 'Imie',
+			'lastname' => 'Nazwisko',
+			'email' => 'borusiewicz.radek@gmail.com',
+			'URLC' => 'http://v-ie.uek.krakow.pl/~s175714/app_dev.php/payment/handle'
+		);
+		
+		$params = http_build_query($data);
+		
+		$url = sprintf(
+			'%s?%s',
+			'https://ssl.dotpay.pl/',
+			$params
+		);
+		
+		return new RedirectResponse ($url);
+    }
+	
+	public function handleAction(Request $request)
+	{
+		$logger = $this->get('monolog.logger.dotpay');
+		$logger->info('===== NEW URLC NOTIFICATION =======');
+		$logger->info(var_export($request->request, true));
+		//postman rest client
+		$response = $this->get('payment.handler')
+		->handleRequest($request)
+		;
+		return new Response($response);
+	}
+}
