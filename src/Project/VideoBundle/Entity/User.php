046188ddceecfadce2f711e88a2bf3eea17d977a
rbor <root@brad-ThinkPad-X131e.(none)>
2015-02-04 21:51:01 +0100

    update User.php

--- src/Project/VideoBundle/Entity/User.php
+++ src/Project/VideoBundle/Entity/User.php
@@ -19,6 +19,60 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+    * @ORM\Column(type="string", length=255)
+    */
+    protected $firstname;
+
+    /**
+    * @ORM\Column(type="string", length=255)
+    */
+    protected $lastname;
+
+    /**
+    * Set firstname
+    *
+    * @param string $firstname
+    * @return User
+    */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+    * Get firstname
+    *
+    * @return string
+    */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+        /**
+    * Set lastname
+    *
+    * @param string $lastname
+    * @return User
+    */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+    * Get lastname
+    *
+    * @return string
+    */
+    public function getLasttname()
+    {
+        return $this->lastname;
+    }
+
     public function __construct()
     {
         parent::__construct();
