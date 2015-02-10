5f225e85553766cd8beb35c7b5d266cac95bccdc
Marcin Wegrzyn <mar512@interia.pl>
2015-02-10 19:39:50 +0100

    dodawanie filmow do bazy

--- src/Project/VideoBundle/Controller/MovieController.php
+++ src/Project/VideoBundle/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace Project\VideoBundle\Controller;
 
+use Project\VideoBundle\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MovieController extends Controller
@@ -67,6 +68,33 @@ class MovieController extends Controller
     public function findAction()
     {
         return $this->render('ProjectVideoBundle:Movie:find.html.twig');
+    }
+
+    public function createDatabaseStuffAction(){
+        $movies = array(
+            'titanic'       => 'tt0120338',
+            'braveheart'    => 'tt0112573',
+            'matrix'        => 'tt0106062',
+            'django'        => 'tt0060315',
+            'goodfellas'    => 'tt0099685',
+            'inception'     => 'tt1375666',
+            'bogowie'       => 'tt3745620',
+            'gladiator'     => 'tt0172495',
+            'casino'        => 'tt0112641',
+            'alien'         => 'tt0078748'
+        );
+
+        $em = $this->getDoctrine()->getManager();
+
+        foreach ($movies as $key => $value) {
+            $movie = new Movie();
+            $movie->setImdbId($value);
+            $movie->setPrice(20.0);
+            $em->persist($movie);
+            $em->flush();
+        }
+
+        return $this->render('ProjectVideoBundle:Movie:done.html.twig');
     }
 
 }
